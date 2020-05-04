e57ef6182eb4500b2e9dda37e931b6019d0c9af0
Mittul Singh <mittul.singh@aalto.fi>
2020-05-04 12:05:29 +0300

    added attention layer and modified path

--- neural_networks.py
+++ neural_networks.py
@@ -1773,3 +1773,90 @@ class SimpleActivationLayer(nn.Module):
     def forward(self, x):
         return self.act(x)
 
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v):
+
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1, add_residual=False):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.add_residual = add_residual
+
+
+    def forward(self, q, k, v):
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+
+        residual = torch.zeros_like(q)         
+        if self.add_residual:
+            residual = q
+            q = self.layer_norm(q)
+
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x n x dv
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # Transpose for attention dot product: b x n x lq x dv
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+        
+        q = self.attention(q, k, v)
+
+        # Transpose to move the head dimension back: b x lq x n x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
+        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        q = self.dropout(self.fc(q))
+        q += residual
+
+        return q
+
+class AttentionLayer(nn.Module):
+    ''' Attend to the input layers  '''
+
+    def __init__(self, options, inp_dim):
+        super(AttentionLayer, self).__init__()
+        self.input_dim = inp_dim
+        self.n_comb = int(options["att_n_comb"])
+        self.n_head = int(options["att_n_head"])
+        self.d_k = int(options["att_key_dim"])
+        self.d_v = int(options["att_value_dim"])
+        self.dropout = float(options["att_dropout"])
+        self.add_residual = strtobool(options["att_add_residual"])
+        
+        self.slf_attn = MultiHeadAttention(self.n_head, self.input_dim, self.d_k, self.d_v, self.dropout, self.add_residual)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], self.n_comb, -1)
+        out = self.slf_attn(x, x, x)
+        return out.view(x.shape[0], -1)
